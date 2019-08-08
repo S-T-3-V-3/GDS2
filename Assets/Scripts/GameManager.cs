5b1332d97035d328cc4134fcb9cdff4b70f21c16
S_T_3_V_3 <steve@st3v3.com>
2019-08-08 17:27:50 +1000

    As a player, I want to be represented by a player character

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class GameManager : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
