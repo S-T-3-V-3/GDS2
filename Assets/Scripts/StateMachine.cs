68cfb16ec421f637222292880590e0db0c34cd39
S_T_3_V_3 <steve@st3v3.com>
2019-08-27 07:03:21 +1000

    State machine setup

--- Assets/Scripts/StateMachine.cs
+++ Assets/Scripts/StateMachine.cs
@@ -1,11 +1,28 @@
 ﻿using UnityEngine;
 
-public abstract class State : MonoBehaviour
-{
+public abstract class State : MonoBehaviour {
     public abstract void BeginState();
-    public abstract void EndState();
+    public virtual void EndState() {
+        
+    }
 }
 
-public class StateMachine {
+public class StateManager : MonoBehaviour {
+    State currentState;
     
+    public void AddState<T>() where T : State {
+        if (currentState != null) {
+            this.RemoveState();
+        }
+
+        currentState = this.gameObject.AddComponent<T>();
+        currentState.BeginState();
+    }
+
+    public void RemoveState() {
+        if (currentState != null) {
+            currentState.EndState();
+            Component.Destroy(currentState);
+        }
+    }
 }
