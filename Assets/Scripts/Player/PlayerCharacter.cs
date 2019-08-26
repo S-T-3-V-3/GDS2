bf978a11dc3f9bc95374ba07eb0f2c0177ff4112
S_T_3_V_3 <steve@st3v3.com>
2019-08-27 07:08:35 +1000

    Player character and input system refactor

---
+++ Assets/Scripts/Player/PlayerCharacter.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName="Player Character")]
+public class PlayerCharacter : ScriptableObject
+{
+    public Mesh mesh;
+    public Material material;
+}
---
+++ Assets/Scripts/Player/PlayerInputHandler.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerInputHandler : MonoBehaviour
+{
+    //public UnityEvent OnTeamSelectNavigation();
+}
