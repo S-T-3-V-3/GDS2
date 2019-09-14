a260a1b8a31f4c89efecd9f5e49dc1c9c7e62ee1
saddboi <sammatthews6@outlook.com>
2019-09-14 21:49:44 +1000

    Complete UI Overhaul/Revamp
    Completely Revamped In-Game HUD, adding information display for each player's health, score and team. As well as this, added a bar-graph UI element that displays a live ratio of tile ownership per team during gameplay. For As a player, I want a readable User Interface so I can keep track of important game-related parameters.

---
+++ Assets/Scripts/HUD/PlayerHUDPrefab.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class PlayerHUDPrefab : MonoBehaviour
+{
+    public Image playerPortrait;
+    public TextMeshProUGUI playerText;
+    public TextMeshProUGUI teamText;
+    public TextMeshProUGUI pointsText;
+    public RawImage playerHP;
+    public TextMeshProUGUI playerHPText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
