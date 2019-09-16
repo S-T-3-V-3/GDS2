f7bdd31f4c1b56cb82f0218c3580d3a396af95d4
Roald Braathen <roaldbraathen@gmail.com>
2019-09-16 22:10:37 +1000

    As a player I want to be able to choose my player color

---
+++ Assets/playerModel.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerModel : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Light underLight;
+    playerModelEyes eyeScript;
+
+
+    Color[] colors;
+    void Start()
+    {
+        underLight = GetComponentInChildren<Light>();
+        eyeScript = GetComponentInChildren<playerModelEyes>();
+
+        colors = new Color[10];
+        colors[0] = new Color(0.9f, 0, 0);
+        colors[1] = new Color(0, 0.9f, 0);
+        colors[2] = new Color(0, 0.2f, 1);
+        colors[3] = new Color(1, 0.7f, 0);
+        colors[4] = new Color(1, 0, 1);
+        colors[5] = new Color(1, 1, 0);
+        colors[6] = new Color(1, 1, 0);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            Color tempColor = colors[Random.Range(0,6)];
+            SetColor(tempColor);
+            eyeScript.SetEyeColor(tempColor);
+        }
+
+
+        if (Input.GetKeyDown("z"))
+        {
+            setExpression(Random.Range(0, 5));
+            
+        }
+
+    }
+
+    public void SetColor(Color color)
+    {
+        underLight.color = color;
+        //eyeMat.color = new Color(0.5f, 0.2f, 0.3f);
+    }
+
+    public void setExpression(int expression)
+    {
+        switch (expression)
+        {
+            case 1:
+                eyeScript.SetExpressionWake();
+                
+                break;
+            case 2:
+                eyeScript.SetExpressionDie();
+                break;
+            case 3:
+                eyeScript.SetExpressionAngry();
+                break;
+            case 4:
+                eyeScript.SetExpressionScared();
+                break;
+            default:
+                eyeScript.SetExpressionNormal();
+                break;
+        }
+    }
+
+}
---
+++ Assets/playerModelEyes.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerModelEyes : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Material eyeMat;
+    Renderer eyeRenderer;
+    Color currentColor;
+
+    float eyeFrame;
+    float frameSize;
+
+    void Start()
+    {
+        eyeRenderer = gameObject.GetComponent<Renderer>();
+
+        eyeFrame = 0;
+
+        frameSize = 1 / 16;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //myRend.material.color = currentColor;
+
+        //myRend.material.mainTextureOffset = new Vector2(Time.time, Time.time*10);
+        eyeFrame+=.0625f;
+        //print (eyeFrame);
+        //eyeRenderer.materials[0].SetTextureOffset("_BaseColorMap", new Vector2(0, eyeFrame));
+    }
+    public void SetEyeColor(Color color)
+    {
+        currentColor = color;
+        print(color);
+        print(eyeRenderer.name);
+        //myRend.material.shader = Shader.Find("HDRP/Lit");
+        eyeRenderer.material.SetColor("_BaseColor", color);
+
+
+        //myRend.material.mainTextureOffset = new Vector2(.1f, 0);
+
+    }
+    public void SetExpressionWake()
+    {
+        eyeRenderer.materials[0].SetTextureOffset("_BaseColorMap", new Vector2(0, .0625f * 6));
+    }
+
+    public void SetExpressionDie()
+    {
+        eyeRenderer.materials[0].SetTextureOffset("_BaseColorMap", new Vector2(0, .0625f * 7));
+    }
+
+    public void SetExpressionAngry()
+    {
+        eyeRenderer.materials[0].SetTextureOffset("_BaseColorMap", new Vector2(0, .0625f * 2));
+    }
+
+    public void SetExpressionScared()
+    {
+        eyeRenderer.materials[0].SetTextureOffset("_BaseColorMap", new Vector2(0, .0625f * 4));
+    }
+    public void SetExpressionNormal()
+    {
+        eyeRenderer.materials[0].SetTextureOffset("_BaseColorMap", new Vector2(0, .0625f * 8));
+    }
+
+}
